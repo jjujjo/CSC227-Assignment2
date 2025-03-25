b5ec81564595b8ca7ca6d4d666726fa691024393
K0alaxy <nowamokamini@gmail.com>
2025-03-25 23:27:10 +0300

    .

--- Main.java
+++ Main.java
@@ -1,3 +1,85 @@
+import java.util.*;
+
 public class Main {
-    
+  public static void main (String [] args){
+
+    LinkedList<Block> listOfBlocks = new LinkedList<>();
+    MemoryManagement memo = new MemoryManagement(listOfBlocks);
+
+    Scanner read = new Scanner(System.in);
+    System.out.print("Enter the total number of blocks: ");
+    int numOfBlocks = read.nextInt();
+
+    int sizeOfBlock = 0;
+    int startOfNextMemory = 0;
+    System.out.print("Enter the size of each block in KB: ");
+    for (int i = 0 ; i < numOfBlocks ; i++){
+        sizeOfBlock = read.nextInt();
+        Block b = new Block(sizeOfBlock, startOfNextMemory, startOfNextMemory+(sizeOfBlock-1));
+        listOfBlocks.add(b);
+        startOfNextMemory+= sizeOfBlock;
+    }
+
+    System.out.print("Enter allocation strategy (1 for first-fit, 2 for best-fit, 3 for worst-fit): ");
+    int straregy = read.nextInt();
+
+    System.out.println("Memory blocks are created…/nMemory blocks:");
+    System.out.println("============================================");
+    System.out.println("Block#\tSize\tStart-End\tStatus");
+    System.out.println("============================================");
+
+    for (int i = 0; i < listOfBlocks.size(); i++) {
+
+        Block block = listOfBlocks.get(i);
+        System.out.printf("Block%d\t%d\t%d-%d\t\t%s\n",
+
+                i, block.getSize(), block.getStartAddress(), block.getEndAddress(),
+
+                (block.getIsAllocated() ? "Allocated" : "Free"));
+    }
+    System.out.println("============================================");
+
+
+    int choice = 0;
+    String pID;
+    int pSize;
+    do{
+        System.out.print("1) Allocates memory blocks\n2) De-allocates memory blocks\n");
+        System.out.print("3) Print report about the current state of memory and internal Fragmentation\n4) Exit\n");
+        System.out.println("============================================");
+
+        System.out.print("Enter your choice: ");
+        choice = read.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.print("Enter the process ID and size of process: ");
+                pID = read.next();
+                pSize = read.nextInt();
+                memo.allocateProcess(pID,pSize,straregy);
+                break;
+            case 2:
+                System.out.print("Enter the process ID: ");
+                pID = read.next();
+                memo.deAllocateProcess(pID);               
+                break;
+            case 3:
+                memo.printMemoryReport();
+                break;
+            case 4:
+                System.out.println("All memory freed! (o^3^o) Don't forget me~");
+                break;
+            default:
+                System.out.println("invalid input, Try a number from 1 to 4");
+        }
+        System.out.println("============================================");
+
+    } while(choice != 4);
+
+
+
+
+
+
+
+  } 
 }
--- MemoryManagement.java
+++ MemoryManagement.java
@@ -8,6 +8,7 @@ public class MemoryManagement {
     }
 
     public void allocateProcess(String processID, int size, int strategy) {
+        boolean allocatedSuccessfully = false;
         switch (strategy) {
             case 1:
                 for (int i = 0; i < memory.size(); i++) {
@@ -16,11 +17,14 @@ public class MemoryManagement {
                         b.setProcessID(processID);
                         b.setIsAllocated(true);
                         b.setInternalFragmentation(b.getSize() - size);
-                        System.out.println(b.getProcessID() + "allocated at address: " + b.getStartAddress()
+                        System.out.println(b.getProcessID() + " allocated at address: " + b.getStartAddress()
                                 + ", and the internal fragmentation is: " + b.getInternalFragmentation());
+
+                        allocatedSuccessfully = true;
                         break;
                     }
                 }
+                if (!allocatedSuccessfully){System.out.println("There is no space in the memory for your process:(");}
                 break;
 
             case 2:
@@ -32,20 +36,23 @@ public class MemoryManagement {
                         break;
                     }
                 }
-                for (int i = min; i < memory.size(); i++) {
-                    Block b = memory.get(i);
-                    if (b.getIsAllocated() == false && size <= b.getSize() && b.getSize() < min) {
-                        min = i;
+                if (min != -1){
+                    for (int i = min+1; i < memory.size(); i++) {
+                        Block b = memory.get(i);
+                        if (b.getIsAllocated() == false && size <= b.getSize() && b.getSize() < memory.get(min).getSize()) {
+                            min = i;
+                        }
                     }
-                }
-                if (min != -1) {
                     Block b = memory.get(min);
                     b.setProcessID(processID);
                     b.setIsAllocated(true);
                     b.setInternalFragmentation(b.getSize() - size);
-                    System.out.println(b.getProcessID() + "allocated at address: " + b.getStartAddress()
+                    System.out.println(b.getProcessID() + " allocated at address: " + b.getStartAddress()
                             + ", and the internal fragmentation is: " + b.getInternalFragmentation());
                 }
+                else
+                    System.out.println("There is no space in the memory for your process:(");              
+                
                 break;
             case 3:
                 int max = -1;
@@ -56,20 +63,22 @@ public class MemoryManagement {
                         break;
                     }
                 }
-                for (int i = max; i < memory.size(); i++) {
-                    Block b = memory.get(i);
-                    if (b.getIsAllocated() == false && size <= b.getSize() && b.getSize() > max) {
-                        max = i;
-                    }
-                }
-                if (max != -1) {
+                if (max != -1){
+                    for (int i = max+1; i < memory.size(); i++) {
+                        Block b = memory.get(i);
+                        if (b.getIsAllocated() == false && size <= b.getSize() && b.getSize() > memory.get(max).getSize()) {
+                            max = i;
+                        }
+                    }                
                     Block b = memory.get(max);
                     b.setProcessID(processID);
                     b.setIsAllocated(true);
                     b.setInternalFragmentation(b.getSize() - size);
-                    System.out.println(b.getProcessID() + "allocated at address: " + b.getStartAddress()
+                    System.out.println(b.getProcessID() + " allocated at address: " + b.getStartAddress()
                             + ", and the internal fragmentation is: " + b.getInternalFragmentation());
-                }
+                    }
+                    else
+                        System.out.println("There is no space in the memory for your process:(");               
                 break;
 
             default:
@@ -79,29 +88,34 @@ public class MemoryManagement {
     }
 
     public void deAllocateProcess(String processID) {
+        boolean deAllocatedSuccessfully = false;
         for (int i = 0; i < memory.size(); i++) {
             Block b = memory.get(i);
             if (b.getIsAllocated() && processID.equalsIgnoreCase(b.getProcessID())) {
-                b.setProcessID(null);
                 b.setIsAllocated(false);
                 b.setInternalFragmentation(0);
-                System.out.println(
-                        "processID: " + b.getProcessID() + " is deallocated from address " + b.getStartAddress());
+                System.out.println(b.getProcessID() + " is deallocated from address " + b.getStartAddress());
+
+                b.setProcessID(null);
+                deAllocatedSuccessfully = true;
                 break;
             }
+        }
+        if (!deAllocatedSuccessfully){
+            System.out.println("There is no such process with the id " + processID + " has been found in the memory");
         }
     }
 
 public void printMemoryReport() {
 
-    System.out.println("===========================================================");
-    System.out.println("Block#\tSize\tStart-End\tStatus\tProcessID\tFragmentation");
-    System.out.println("===========================================================");
+    System.out.println("=================================================================================");
+    System.out.println("Block#\tSize\tStart-End\tStatus\t\tProcessID\tFragmentation");
+    System.out.println("=================================================================================");
 
     for (int i = 0; i < memory.size(); i++) {
 
         Block block = memory.get(i);
-        System.out.printf("%d\t%d\t%d-%d\t%s\t%s\t%d\n",
+        System.out.printf("Block%d\t%d\t%d-%d\t\t%-10s\t%s\t\t%d\n",
 
                 i, block.getSize(), block.getStartAddress(), block.getEndAddress(),
 
@@ -109,6 +123,6 @@ public void printMemoryReport() {
 
                 block.getProcessID(), block.getInternalFragmentation());
     }
-    System.out.println("===========================================================");
+    System.out.println("=================================================================================");
 }
 }
